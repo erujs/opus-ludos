b4213c10c7565fbb8ded0b720dcfd96f1e2f1d37
unknown <ljrsqn@gmail.com>
2021-01-15 23:28:55 +0800

    class to functional components migration

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://project-novus-83ffc.firebaseio.com/'
+    baseURL: 'https://project-novus-83ffc.firebaseio.com/'
 });
 
 export default instance;
--- src/container/library/library.js
+++ src/container/library/library.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import AppErr from '../../AppErr';
 import * as actions from '../../store/actions/index';
 import Admin from '../../components/admin/admin';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
-class Library extends Component {
-    componentDidMount() {
-        this.props.onInitData();
-        this.props.onInitGenre();
-    }
-    dataRender() {
-        switch(this.props.status){
+const Library = (props) => {
+    useEffect(() => {
+        props.onInitData();
+        props.onInitGenre();
+    })
+    let dataRender = () => {
+        switch(props.status){
             case 200:
                 return(
                     <Admin />
@@ -28,9 +28,7 @@ class Library extends Component {
                 return <LinearProgress />
         }
     }
-    render() {
-        return this.dataRender();
-    }
+    return dataRender();
 }
 
 const mapStateToProps = state => {
