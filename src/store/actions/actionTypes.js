6484bad97939c6e43e48798067e1db23b76b49c9
erujs <a.m.jerus@gmail.com>
2024-03-12 23:33:58 +0800

    revamp to vite

--- src/store/actions/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const SET_DATA_GAMES = 'SET_DATA_GAMES';
-export const SET_GENRE = 'SET_GENRE';
-export const ERROR = 'ERROR';
--- src/store/actions/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://project-ludos-default-rtdb.asia-southeast1.firebasedatabase.app/'
-});
-
-export default instance;
